4e19bd2a109c70641ad2aadad5c081737f55c3ba
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-02 19:50:19 +0300

    add function multiple

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def minus(a, b):
     return a - b
 
 
+def multiple(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     print(plus(2, 3))
     print(minus(4, 3))
+    print(multiple(2, 3))
